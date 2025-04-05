7b0bac22fc91cd6d399684b6d62b6fda0f943680
Angel kant <kantangel48@gmail.com>
2025-04-05 12:53:44 -0400

    backend main working

--- Backend/main.py
+++ Backend/main.py
@@ -1,1 +1,14 @@
 from fastapi import FastAPI
+import psutil
+
+app =   FastAPI()
+
+@app.get("/stats")
+
+def get_stats():
+    return {
+        "cpu_percent": psutil.cpu_percent(interval=1),
+        "ram": psutil.virtual_memory()._asdict(),
+        "disk": psutil.disk_usage("/")._asdict(),
+        "net_io": psutil.net_io_counters()._asdict()
+    }
